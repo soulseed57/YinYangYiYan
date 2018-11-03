afe6cac17b14beeaaef62df9042b384538b165ae
Wangshiqi <xinke57@sina.com>
2018-11-03 13:35:08 +0800

    增加顺逆排序方法

--- src/阴阳易演/引用库/常用方法.cs
+++ src/阴阳易演/引用库/常用方法.cs
@@ -20,5 +20,33 @@
                 list.Insert(0, item);
             }
         }
+
+        public static T[] 顺逆排序<T>(bool isOrder, T[] array)
+        {
+            var list = new List<T>();
+            var len = array.Length;
+            for (var i = 0; i < len; i++)
+            {
+                int index;
+                if (isOrder)
+                {
+                    index = i;
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        index = i;
+                    }
+                    else
+                    {
+                        index = len - i;
+                    }
+                }
+                list.Add(array[index]);
+            }
+            return list.ToArray();
+        }
+
     }
 }
