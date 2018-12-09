d7a7407bbe743e8f11f1e381e2d8c31e577cf4ca
Wangshiqi <xinke57@sina.com>
2018-12-09 16:10:00 +0800

    增加八方方位和十六方位

--- src/阴阳易演/抽象类/八卦.cs
+++ src/阴阳易演/抽象类/八卦.cs
@@ -37,8 +37,8 @@ namespace 阴阳易演.抽象类
         //阳动
         public int 序数 { get; protected set; }
         public int 本数 { get; protected set; }
-        public 十六方位 卦位 { get; protected set; }
-        public 十六方位 方位 { get; protected set; }
+        public 八方方位 卦位 { get; protected set; }
+        public 八方方位 方位 { get; protected set; }
         public byte 卦值 { get; protected set; }
         public 四象 四象 { get; protected set; }
         public string 人物 { get; protected set; }
