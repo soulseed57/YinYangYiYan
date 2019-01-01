f50e149b64ae3a6affa73a44e029ef9c5489eb4a
Wangshiqi <xinke57@sina.com>
2019-01-01 17:48:05 +0800

    将类库整理为共享项目,并对应生成NetStandard和NetFramework类

--- src/YinYangYiYan.Test/UnitTest2.cs
+++ src/YinYangYiYan.Test/UnitTest2.cs
@@ -5,6 +5,7 @@
     using System.Text;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using 阴阳易演.具象类.五行;
+    using 阴阳易演.容器类;
     using 阴阳易演.引用库;
     using 阴阳易演.抽象类;
     using 阴阳易演.查询类;
@@ -47,55 +48,23 @@
         public void 五行旺衰()
         {
             var showMsg = new StringBuilder();
-            var 季节列表 = new List<季节> { 季节.春季, 季节.夏季, 季节.秋季, 季节.冬季, 季节.四季 };
+            var 季节列表 = new List<季节> { 季节.春季, 季节.夏季, 季节.秋季, 季节.冬季, 季节.长夏 };
             var 五行列表 = new List<五行> { 五行.金, 五行.木, 五行.水, 五行.火, 五行.土 };
             foreach (var 季 in 季节列表)
             {
-                showMsg.Append($"{常用方法.获取类名(季)}\r\n");
+                showMsg.Append($"{季.名称}\r\n");
                 foreach (var 行 in 五行列表)
                 {
-                    showMsg.Append($"{常用方法.获取类名(行)}{季 + 行}\t");
+                    showMsg.Append($"{行.名称}{季 + 行}\t");
                 }
                 showMsg.AppendLine();
-                showMsg.Append($"{常用方法.获取类名(季.旺)}旺\t");
-                showMsg.Append($"{常用方法.获取类名(季.相)}相\t");
-                showMsg.Append($"{常用方法.获取类名(季.休)}休\t");
-                showMsg.Append($"{常用方法.获取类名(季.囚)}囚\t");
-                showMsg.Append($"{常用方法.获取类名(季.死)}死\t");
+                showMsg.Append($"{季.旺.名称}旺\t");
+                showMsg.Append($"{季.相.名称}相\t");
+                showMsg.Append($"{季.休.名称}休\t");
+                showMsg.Append($"{季.囚.名称}囚\t");
+                showMsg.Append($"{季.死.名称}死\t");
                 showMsg.AppendLine();
             }
-            Console.Write(showMsg.ToString());
-        }
-
-        [TestMethod]
-        public void 天干阴阳()
-        {
-            var showMsg = new StringBuilder();
-            foreach (var 干 in 干支表.阳干表)
-            {
-                showMsg.Append($"{常用方法.获取类名(干.阴阳)}");
-                showMsg.Append($"{常用方法.获取类名(干.五行)}\t");
-            }
-            showMsg.AppendLine();
-            foreach (var 干 in 干支表.阴干表)
-            {
-                showMsg.Append($"{常用方法.获取类名(干.阴阳)}");
-                showMsg.Append($"{常用方法.获取类名(干.五行)}\t");
-            }
-            showMsg.AppendLine();
-
-            var 天干干支 = new List<天干>
-            {
-                天干.甲,天干.丙,天干.戊,天干.庚,天干.壬,
-                天干.乙,天干.丁,天干.己,天干.辛,天干.癸
-            };
-            foreach (var 干 in 天干干支)
-            {
-                showMsg.Append($"{常用方法.获取类名(干.五行)} ");
-                showMsg.Append($"{常用方法.获取类名(干.阴阳)}\t");
-            }
-            showMsg.AppendLine();
-
             Console.Write(showMsg.ToString());
         }
 
@@ -109,24 +78,8 @@
             {
                 var 干 = 天干表[i % 天干表.Count];
                 var 支 = 地支表[i % 地支表.Count];
-                var 甲子 = 干支关系.六十甲子(干, 支);
+                var 甲子 = new 甲子(干, 支);
                 showMsg.Append($"{甲子.名称}{甲子.序数:D2}\t");
-                if ((i + 1) % 10 == 0) showMsg.Append("\r\n");
-            }
-            Console.Write(showMsg.ToString());
-        }
-
-        [TestMethod]
-        public void 十二长生()
-        {
-            var showMsg = new StringBuilder();
-            var 天干表 = 干支表.天干表;
-            var 地支表 = 干支表.地支表;
-            for (var i = 0; i < 60; i++)
-            {
-                var 干 = 天干表[i % 天干表.Length];
-                var 支 = 地支表[i % 地支表.Length];
-                showMsg.Append($"{常用方法.获取类名(干)}在{常用方法.获取类名(支)}:{干支关系.十二长生(干, 支)}\t");
                 if ((i + 1) % 10 == 0) showMsg.Append("\r\n");
             }
             Console.Write(showMsg.ToString());
@@ -190,7 +143,7 @@
             var 甲子 = new 干支纪年(时间);
             showMsg.AppendLine($"阴历:{甲子.阴历年}年{甲子.阴历月}月{甲子.阴历日}日");
             showMsg.AppendLine($"{甲子.年柱.名称}年 【{甲子.年柱.地支.生肖}年】");
-            showMsg.AppendLine($"{甲子.月柱.名称}月 {甲子.日柱.名称}日 {甲子.时辰.GetType().Name}时");
+            showMsg.AppendLine($"{甲子.月柱.名称}月 {甲子.日柱.名称}日 {甲子.时柱.名称}时");
             //阴历月份转名称
             for (var i = 1; i <= 12; i++)
             {
@@ -204,19 +157,6 @@
             }
             showMsg.AppendLine();
 
-            Console.Write(showMsg.ToString());
-        }
-
-        [TestMethod]
-        public void 时辰推算()
-        {
-            var showMsg = new StringBuilder();
-            for (var i = 0; i < 24; i++)
-            {
-                var t = (i + 23) % 24;
-                var time = 干支纪年.计算时辰(t).GetType().Name;
-                showMsg.AppendLine($"{t:D2}点 {time}时");
-            }
             Console.Write(showMsg.ToString());
         }
 
@@ -286,32 +226,6 @@
         }
 
         [TestMethod]
-        public void 节气测试()
-        {
-            var showMsg = new StringBuilder();
-            var date = DateTime.Now;
-            showMsg.AppendLine($"当前节气:{二十四节气.节气查询(date)}");
-
-            //修正儒略日测试
-            var mjd = 二十四节气.日期转修正儒略日(date) ?? 0;
-            var ymd = 二十四节气.修正儒略日转日期(mjd);
-            showMsg.AppendLine($"日期:{date:yyyy/MM/dd}");
-            showMsg.AppendLine($"修正儒略日:{mjd}");
-            showMsg.AppendLine($"转换回日期:{ymd?.Year}/{ymd?.Month}/{ymd?.Day}");
-            //节气判断测试
-            var check = new DateTime(2017, 10, 7);
-            showMsg.AppendLine($"查询时间:{check:yyyy/MM/dd},节气:{二十四节气.节气查询(check)}");
-            //节气查询测试
-            var st = 二十四节气.节气枚举.冬至;
-            var stDate = 二十四节气.节气查询(date.Year, st);
-            showMsg.AppendLine($"查询节气:{st},时间:{stDate.Year}-{stDate.Month}-{stDate.Day}");
-            //节气设置测试
-            var jq = new 二十四节气(2018, 二十四节气.节气枚举.立春);
-            showMsg.AppendLine($"查询节气:{jq.名称} 日期:{jq.日期:yyyy-MM-dd}");
-            Console.Write(showMsg.ToString());
-        }
-
-        [TestMethod]
         public void 年月大写测试()
         {
             var showMsg = new StringBuilder();
@@ -331,27 +245,6 @@
             monthName = 干支纪年.月份名称(jz.阴历月);
             dayName = 干支纪年.日期名称(jz.阴历日);
             showMsg.AppendLine($"阴历年:{yearName}{monthName}{dayName}");
-
-            Console.Write(showMsg.ToString());
-        }
-
-        [TestMethod]
-        public void 四季判断()
-        {
-            var showMsg = new StringBuilder();
-            for (var i = 0; i < 24; i++)
-            {
-                var 节气 = (二十四节气.节气枚举)Enum.ToObject(typeof(二十四节气.节气枚举), i);
-                var 时间 = 二十四节气.节气查询(2017, 节气);
-                var 季节名 = 季节.季节判断(时间).GetType().Name;
-                showMsg.AppendLine($"{节气}:{时间:MM-dd}\t{季节名}");
-            }
-            var t = new DateTime(2017, 1, 1);
-            var r = new Random();
-            var d = r.Next(365);
-            var date = t.AddDays(d);
-            var season = 季节.季节判断(date).GetType().Name;
-            showMsg.AppendLine($"{date:yyyy-MM-dd}\t{season}");
 
             Console.Write(showMsg.ToString());
         }
--- src/YinYangYiYan.Test/UnitTest3.cs
+++ src/YinYangYiYan.Test/UnitTest3.cs
@@ -31,43 +31,24 @@
             var date = DateTime.Now;
             var 纪年 = new 干支纪年(date);
             Console.WriteLine($"时间:{date:yyyy年 MM月 dd日 HH时}");
-            Console.WriteLine($"{纪年.年柱.名称}年 {纪年.月柱.名称}月 {纪年.日柱.名称}日 {常用方法.获取类名(纪年.时辰)}时");
-            Console.WriteLine($"旬空:{常用方法.获取类名(纪年.旬空[0])}{常用方法.获取类名(纪年.旬空[1])}空");
+            Console.WriteLine($"{纪年.年柱.名称}年 {纪年.月柱.名称}月 {纪年.日柱.名称}日 {纪年.时柱.名称}时");
+            Console.WriteLine($"旬空:{纪年.旬空[0].名称}{纪年.旬空[1].名称}空");
             Console.WriteLine();
 
             date = new DateTime(1904, 1, 30);
             纪年 = new 干支纪年(date);
             Console.WriteLine($"时间:{date:yyyy年 MM月 dd日 HH时}");
-            Console.WriteLine($"{纪年.年柱.名称}年 {纪年.月柱.名称}月 {纪年.日柱.名称}日 {常用方法.获取类名(纪年.时辰)}时");
-            Console.WriteLine($"旬空:{常用方法.获取类名(纪年.旬空[0])}{常用方法.获取类名(纪年.旬空[1])}空");
+            Console.WriteLine($"{纪年.年柱.名称}年 {纪年.月柱.名称}月 {纪年.日柱.名称}日 {纪年.时柱.名称}时");
+            Console.WriteLine($"旬空:{纪年.旬空[0].名称}{纪年.旬空[1].名称}空");
             Console.WriteLine();
 
             date = new DateTime(1904, 1, 31);
             纪年 = new 干支纪年(date);
             Console.WriteLine($"时间:{date:yyyy年 MM月 dd日 HH时}");
-            Console.WriteLine($"{纪年.年柱.名称}年 {纪年.月柱.名称}月 {纪年.日柱.名称}日 {常用方法.获取类名(纪年.时辰)}时");
-            Console.WriteLine($"旬空:{常用方法.获取类名(纪年.旬空[0])}{常用方法.获取类名(纪年.旬空[1])}空");
+            Console.WriteLine($"{纪年.年柱.名称}年 {纪年.月柱.名称}月 {纪年.日柱.名称}日 {纪年.时柱.名称}时");
+            Console.WriteLine($"旬空:{纪年.旬空[0].名称}{纪年.旬空[1].名称}空");
             Console.WriteLine(二十四节气.节气查询(new DateTime(2000, 4, 3)));
-
-            Console.WriteLine($"公元2018年年干:{阴阳易演.计算类.干支纪年.计算年柱(2018).名称}");
-            Console.WriteLine($"公元前479年年干:{阴阳易演.计算类.干支纪年.计算年柱(-479).名称}");
-            Console.WriteLine($"公元0年年干:{阴阳易演.计算类.干支纪年.计算年柱(0).名称}");
-            Console.WriteLine();
-            var startDay = new DateTime(2000, 1, 1);
-
-            for (var i = 0; i <= 120; i++)
-            {
-                //干支纪年 = new 干支纪年(startDay);
-                //Console.WriteLine($"{startDay.Year}-{startDay.Month}-{startDay.Day}\t" +
-                //                  $"{干支纪年.阴历年}-{干支纪年.阴历月}-{干支纪年.阴历日}\t" +
-                //                  $"{干支纪年.年柱.名称}年{干支纪年.月柱.名称}月{干支纪年.日柱.名称}日");
-
-                var 阴历计算 = new ChineseLunisolarCalendar();
-                Console.WriteLine(
-                    $"{阴历计算.GetYear(startDay)}-{阴历计算.GetMonth(startDay)}-{阴历计算.GetDayOfMonth(startDay)}\t");
-                startDay = startDay.AddDays(1);
-            }
-
+            
         }
 
         [TestMethod]
@@ -76,7 +57,7 @@
             var count = 0;
             for (var i = 0; i > -120; i--)
             {
-                var 序数 = 枚举转换类<甲子表.甲子枚举>.序数取余(i, 60);
+                var 序数 = 常用方法.序数取余(i, 60);
                 Console.Write(甲子表.甲子查询(i).名称);
                 Console.Write($"{序数}\t");
                 if (++count % 10 == 0) Console.WriteLine();
@@ -89,7 +70,7 @@
             var count = 0;
             for (var i = 0; i < 120; i++)
             {
-                var 序数 = 枚举转换类<甲子表.甲子枚举>.序数取余(i, 60);
+                var 序数 = 常用方法.序数取余(i, 60);
                 Console.Write(甲子表.甲子查询(i).名称);
                 Console.Write($"{序数}\t");
                 if (++count % 10 == 0) Console.WriteLine();
