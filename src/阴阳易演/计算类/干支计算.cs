a7a63190049d5a7b02529ddd48c0556a39daf303
Wangshiqi <xinke57@sina.com>
2019-04-17 09:52:47 +0800

    1.去掉甲木这种复合类,因为天干里已经包含了五行 2.将五鼠遁和五虎遁归为天干扩展计算,不需要引用静态类了 3.增加了十二长生枚举,统一管理标准名称

--- src/阴阳易演/计算类/干支计算.cs
+++ src/阴阳易演/计算类/干支计算.cs
@@ -5,164 +5,157 @@ namespace 阴阳易演.计算类
     using 具象类.地支;
     using 具象类.天干;
     using 抽象类;
+    using 枚举类;
     using 查询类;
 
     public static class 干支计算
     {
         #region 计算
-        public static string 十二长生(天干 干, 地支 支)
+        public static 十二长生 长生(天干 干, 地支 支)
         {
-            var 结果 = String.Empty;
             switch (干)
             {
-                case 甲木 _ when 支 is 亥:
-                case 丙火 _ when 支 is 寅:
-                case 戊土 _ when 支 is 寅:
-                case 庚金 _ when 支 is 巳:
-                case 壬水 _ when 支 is 申:
-                case 乙木 _ when 支 is 午:
-                case 丁火 _ when 支 is 酉:
-                case 己土 _ when 支 is 酉:
-                case 辛金 _ when 支 is 子:
-                case 癸水 _ when 支 is 卯:
-                    结果 = "长生";
-                    break;
-                case 甲木 _ when 支 is 子:
-                case 丙火 _ when 支 is 卯:
-                case 戊土 _ when 支 is 卯:
-                case 庚金 _ when 支 is 午:
-                case 壬水 _ when 支 is 酉:
-                case 乙木 _ when 支 is 巳:
-                case 丁火 _ when 支 is 申:
-                case 己土 _ when 支 is 申:
-                case 辛金 _ when 支 is 亥:
-                case 癸水 _ when 支 is 寅:
-                    结果 = "沐浴";
-                    break;
-                case 甲木 _ when 支 is 丑:
-                case 丙火 _ when 支 is 辰:
-                case 戊土 _ when 支 is 辰:
-                case 庚金 _ when 支 is 未:
-                case 壬水 _ when 支 is 戌:
-                case 乙木 _ when 支 is 辰:
-                case 丁火 _ when 支 is 未:
-                case 己土 _ when 支 is 未:
-                case 辛金 _ when 支 is 戌:
-                case 癸水 _ when 支 is 丑:
-                    结果 = "冠带";
-                    break;
-                case 甲木 _ when 支 is 寅:
-                case 丙火 _ when 支 is 巳:
-                case 戊土 _ when 支 is 巳:
-                case 庚金 _ when 支 is 申:
-                case 壬水 _ when 支 is 亥:
-                case 乙木 _ when 支 is 卯:
-                case 丁火 _ when 支 is 午:
-                case 己土 _ when 支 is 午:
-                case 辛金 _ when 支 is 酉:
-                case 癸水 _ when 支 is 子:
-                    结果 = "临官";
-                    break;
-                case 甲木 _ when 支 is 卯:
-                case 丙火 _ when 支 is 午:
-                case 戊土 _ when 支 is 午:
-                case 庚金 _ when 支 is 酉:
-                case 壬水 _ when 支 is 子:
-                case 乙木 _ when 支 is 寅:
-                case 丁火 _ when 支 is 巳:
-                case 己土 _ when 支 is 巳:
-                case 辛金 _ when 支 is 申:
-                case 癸水 _ when 支 is 亥:
-                    结果 = "帝旺";
-                    break;
-                case 甲木 _ when 支 is 辰:
-                case 丙火 _ when 支 is 未:
-                case 戊土 _ when 支 is 未:
-                case 庚金 _ when 支 is 戌:
-                case 壬水 _ when 支 is 丑:
-                case 乙木 _ when 支 is 丑:
-                case 丁火 _ when 支 is 辰:
-                case 己土 _ when 支 is 辰:
-                case 辛金 _ when 支 is 未:
-                case 癸水 _ when 支 is 戌:
-                    结果 = "衰";
-                    break;
-                case 甲木 _ when 支 is 巳:
-                case 丙火 _ when 支 is 申:
-                case 戊土 _ when 支 is 申:
-                case 庚金 _ when 支 is 亥:
-                case 壬水 _ when 支 is 寅:
-                case 乙木 _ when 支 is 子:
-                case 丁火 _ when 支 is 卯:
-                case 己土 _ when 支 is 卯:
-                case 辛金 _ when 支 is 午:
-                case 癸水 _ when 支 is 酉:
-                    结果 = "病";
-                    break;
-                case 甲木 _ when 支 is 午:
-                case 丙火 _ when 支 is 酉:
-                case 戊土 _ when 支 is 酉:
-                case 庚金 _ when 支 is 子:
-                case 壬水 _ when 支 is 卯:
-                case 乙木 _ when 支 is 亥:
-                case 丁火 _ when 支 is 寅:
-                case 己土 _ when 支 is 寅:
-                case 辛金 _ when 支 is 巳:
-                case 癸水 _ when 支 is 申:
-                    结果 = "死";
-                    break;
-                case 甲木 _ when 支 is 未:
-                case 丙火 _ when 支 is 戌:
-                case 戊土 _ when 支 is 戌:
-                case 庚金 _ when 支 is 丑:
-                case 壬水 _ when 支 is 辰:
-                case 乙木 _ when 支 is 戌:
-                case 丁火 _ when 支 is 丑:
-                case 己土 _ when 支 is 丑:
-                case 辛金 _ when 支 is 辰:
-                case 癸水 _ when 支 is 未:
-                    结果 = "墓";
-                    break;
-                case 甲木 _ when 支 is 申:
-                case 丙火 _ when 支 is 亥:
-                case 戊土 _ when 支 is 亥:
-                case 庚金 _ when 支 is 寅:
-                case 壬水 _ when 支 is 巳:
-                case 乙木 _ when 支 is 酉:
-                case 丁火 _ when 支 is 子:
-                case 己土 _ when 支 is 子:
-                case 辛金 _ when 支 is 卯:
-                case 癸水 _ when 支 is 午:
-                    结果 = "绝";
-                    break;
-                case 甲木 _ when 支 is 酉:
-                case 丙火 _ when 支 is 子:
-                case 戊土 _ when 支 is 子:
-                case 庚金 _ when 支 is 卯:
-                case 壬水 _ when 支 is 午:
-                case 乙木 _ when 支 is 申:
-                case 丁火 _ when 支 is 亥:
-                case 己土 _ when 支 is 亥:
-                case 辛金 _ when 支 is 寅:
-                case 癸水 _ when 支 is 巳:
-                    结果 = "胎";
-                    break;
-                case 甲木 _ when 支 is 戌:
-                case 丙火 _ when 支 is 丑:
-                case 戊土 _ when 支 is 丑:
-                case 庚金 _ when 支 is 辰:
-                case 壬水 _ when 支 is 未:
-                case 乙木 _ when 支 is 未:
-                case 丁火 _ when 支 is 戌:
-                case 己土 _ when 支 is 戌:
-                case 辛金 _ when 支 is 丑:
-                case 癸水 _ when 支 is 辰:
-                    结果 = "养";
-                    break;
+                case 甲 _ when 支 is 亥:
+                case 丙 _ when 支 is 寅:
+                case 戊 _ when 支 is 寅:
+                case 庚 _ when 支 is 巳:
+                case 壬 _ when 支 is 申:
+                case 乙 _ when 支 is 午:
+                case 丁 _ when 支 is 酉:
+                case 己 _ when 支 is 酉:
+                case 辛 _ when 支 is 子:
+                case 癸 _ when 支 is 卯:
+                    return 十二长生.长生;
+                case 甲 _ when 支 is 子:
+                case 丙 _ when 支 is 卯:
+                case 戊 _ when 支 is 卯:
+                case 庚 _ when 支 is 午:
+                case 壬 _ when 支 is 酉:
+                case 乙 _ when 支 is 巳:
+                case 丁 _ when 支 is 申:
+                case 己 _ when 支 is 申:
+                case 辛 _ when 支 is 亥:
+                case 癸 _ when 支 is 寅:
+                    return 十二长生.沐浴;
+                case 甲 _ when 支 is 丑:
+                case 丙 _ when 支 is 辰:
+                case 戊 _ when 支 is 辰:
+                case 庚 _ when 支 is 未:
+                case 壬 _ when 支 is 戌:
+                case 乙 _ when 支 is 辰:
+                case 丁 _ when 支 is 未:
+                case 己 _ when 支 is 未:
+                case 辛 _ when 支 is 戌:
+                case 癸 _ when 支 is 丑:
+                    return 十二长生.冠带;
+                case 甲 _ when 支 is 寅:
+                case 丙 _ when 支 is 巳:
+                case 戊 _ when 支 is 巳:
+                case 庚 _ when 支 is 申:
+                case 壬 _ when 支 is 亥:
+                case 乙 _ when 支 is 卯:
+                case 丁 _ when 支 is 午:
+                case 己 _ when 支 is 午:
+                case 辛 _ when 支 is 酉:
+                case 癸 _ when 支 is 子:
+                    return 十二长生.临官;
+                case 甲 _ when 支 is 卯:
+                case 丙 _ when 支 is 午:
+                case 戊 _ when 支 is 午:
+                case 庚 _ when 支 is 酉:
+                case 壬 _ when 支 is 子:
+                case 乙 _ when 支 is 寅:
+                case 丁 _ when 支 is 巳:
+                case 己 _ when 支 is 巳:
+                case 辛 _ when 支 is 申:
+                case 癸 _ when 支 is 亥:
+                    return 十二长生.帝旺;
+                case 甲 _ when 支 is 辰:
+                case 丙 _ when 支 is 未:
+                case 戊 _ when 支 is 未:
+                case 庚 _ when 支 is 戌:
+                case 壬 _ when 支 is 丑:
+                case 乙 _ when 支 is 丑:
+                case 丁 _ when 支 is 辰:
+                case 己 _ when 支 is 辰:
+                case 辛 _ when 支 is 未:
+                case 癸 _ when 支 is 戌:
+                    return 十二长生.衰;
+                case 甲 _ when 支 is 巳:
+                case 丙 _ when 支 is 申:
+                case 戊 _ when 支 is 申:
+                case 庚 _ when 支 is 亥:
+                case 壬 _ when 支 is 寅:
+                case 乙 _ when 支 is 子:
+                case 丁 _ when 支 is 卯:
+                case 己 _ when 支 is 卯:
+                case 辛 _ when 支 is 午:
+                case 癸 _ when 支 is 酉:
+                    return 十二长生.病;
+                case 甲 _ when 支 is 午:
+                case 丙 _ when 支 is 酉:
+                case 戊 _ when 支 is 酉:
+                case 庚 _ when 支 is 子:
+                case 壬 _ when 支 is 卯:
+                case 乙 _ when 支 is 亥:
+                case 丁 _ when 支 is 寅:
+                case 己 _ when 支 is 寅:
+                case 辛 _ when 支 is 巳:
+                case 癸 _ when 支 is 申:
+                    return 十二长生.死;
+                case 甲 _ when 支 is 未:
+                case 丙 _ when 支 is 戌:
+                case 戊 _ when 支 is 戌:
+                case 庚 _ when 支 is 丑:
+                case 壬 _ when 支 is 辰:
+                case 乙 _ when 支 is 戌:
+                case 丁 _ when 支 is 丑:
+                case 己 _ when 支 is 丑:
+                case 辛 _ when 支 is 辰:
+                case 癸 _ when 支 is 未:
+                    return 十二长生.墓;
+                case 甲 _ when 支 is 申:
+                case 丙 _ when 支 is 亥:
+                case 戊 _ when 支 is 亥:
+                case 庚 _ when 支 is 寅:
+                case 壬 _ when 支 is 巳:
+                case 乙 _ when 支 is 酉:
+                case 丁 _ when 支 is 子:
+                case 己 _ when 支 is 子:
+                case 辛 _ when 支 is 卯:
+                case 癸 _ when 支 is 午:
+                    return 十二长生.绝;
+                case 甲 _ when 支 is 酉:
+                case 丙 _ when 支 is 子:
+                case 戊 _ when 支 is 子:
+                case 庚 _ when 支 is 卯:
+                case 壬 _ when 支 is 午:
+                case 乙 _ when 支 is 申:
+                case 丁 _ when 支 is 亥:
+                case 己 _ when 支 is 亥:
+                case 辛 _ when 支 is 寅:
+                case 癸 _ when 支 is 巳:
+                    return 十二长生.胎;
+                case 甲 _ when 支 is 戌:
+                case 丙 _ when 支 is 丑:
+                case 戊 _ when 支 is 丑:
+                case 庚 _ when 支 is 辰:
+                case 壬 _ when 支 is 未:
+                case 乙 _ when 支 is 未:
+                case 丁 _ when 支 is 戌:
+                case 己 _ when 支 is 戌:
+                case 辛 _ when 支 is 丑:
+                case 癸 _ when 支 is 辰:
+                    return 十二长生.养;
+                default:
+                    throw new Exception($"未找到地支匹配的长生,当前天干[{干}],当前地支[{支}]");
             }
-            return 结果;
         }
-        public static 天干 五鼠遁(天干 干, 地支 支)
+
+        #endregion
+
+        #region 天干扩展
+        public static 天干 五鼠遁(this 天干 干, 地支 支)
         {
             天干 起;
             switch (干)
@@ -195,7 +188,7 @@ namespace 阴阳易演.计算类
             var 干序 = (序首 + 偏移) % 10;
             return 干支表.天干查询(干序);
         }
-        public static 天干 五虎遁(天干 干, 地支 支)
+        public static 天干 五虎遁(this 天干 干, 地支 支)
         {
             天干 起;
             switch (干)
@@ -228,10 +221,6 @@ namespace 阴阳易演.计算类
             var 干序 = (序首 + 偏移) % 10;
             return 干支表.天干查询(干序);
         }
-
-        #endregion
-
-        #region 扩展
         public static 天干 合(this 天干 干)
         {
             // 甲己合化土，乙庚合化金，丙辛合化水，丁壬合化木，戊癸合化火
@@ -258,7 +247,7 @@ namespace 阴阳易演.计算类
                 case 癸 _:
                     return 天干.戊;
                 default:
-                    throw new Exception($"未找到正确的合化天干,当前输入:{干}");
+                    throw new Exception($"未找到正确的合化天干,当前天干[{干}]");
             }
         }
         public static 五行 化(this 天干 干, 天干 合)
@@ -280,7 +269,64 @@ namespace 阴阳易演.计算类
                 case 戊 _ when 合 is 癸:
                     return 五行.火;// 戊癸合化火
                 default:
-                    throw new Exception($"未找到正确的合化五行,当前输入:{干},{合}");
+                    throw new Exception($"未找到正确的合化五行,当前天干[{干}],合[{合}]");
+            }
+        }
+
+        #endregion
+
+        #region 地支扩展
+        public static 地支 合(this 地支 支)
+        {
+            // 子丑合化土，寅亥合化木，卯戌合化火，辰酉合化金，巳申合化水，午未合化土。
+            switch (支)
+            {
+                case 子 _:
+                    return 地支.丑;
+                case 寅 _:
+                    return 地支.亥;
+                case 卯 _:
+                    return 地支.戌;
+                case 辰 _:
+                    return 地支.酉;
+                case 巳 _:
+                    return 地支.申;
+                case 午 _:
+                    return 地支.未;
+                case 丑 _:
+                    return 地支.子;
+                case 亥 _:
+                    return 地支.寅;
+                case 戌 _:
+                    return 地支.卯;
+                case 酉 _:
+                    return 地支.辰;
+                case 申 _:
+                    return 地支.巳;
+                case 未 _:
+                    return 地支.午;
+                default:
+                    throw new Exception($"未找到正确的合化地支,当前地支[{支}]");
+            }
+        }
+        public static 五行 化(this 地支 支, 地支 合)
+        {
+            switch (支)
+            {
+                case 子 _ when 合 is 丑:
+                    return 五行.土;// 子丑合化土
+                case 寅 _ when 合 is 亥:
+                    return 五行.木;// 寅亥合化木
+                case 卯 _ when 合 is 戌:
+                    return 五行.火;// 卯戌合化火
+                case 辰 _ when 合 is 酉:
+                    return 五行.金;// 辰酉合化金
+                case 巳 _ when 合 is 申:
+                    return 五行.水;// 巳申合化水
+                case 午 _ when 合 is 未:
+                    return 五行.土;// 午未合化土
+                default:
+                    throw new Exception($"未找到正确的合化五行,当前地支[{支}],合[{合}]");
             }
         }
 
