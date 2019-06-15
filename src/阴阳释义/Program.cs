fa6fe70fdb30ce28430b573296d777d014193ad3
Wangshiqi <xinke57@sina.com>
2019-06-15 22:09:01 +0800

    整理项目结构

--- src/阴阳释义/Program.cs
+++ src/阴阳释义/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace 阴阳易演.解析
+namespace 阴阳释义
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
         }
     }
 }
