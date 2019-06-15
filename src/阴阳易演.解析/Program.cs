242197ee503cea6c2120832932027f5b390f2dc6
Wangshiqi <xinke57@sina.com>
2019-06-15 20:15:44 +0800

    添加语法解析项目

---
+++ src/阴阳易演.解析/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace 阴阳易演.解析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
