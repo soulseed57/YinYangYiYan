8097565f4c3fa4428b14a28c8082956de0164b4a
soulseed57 <soulseed57@hotmail.com>
2019-10-13 21:29:48 +0800

    解决当更改枚举值时序数计算错误问题

--- src/阴阳易演/引用库/枚举转换类.cs
+++ src/阴阳易演/引用库/枚举转换类.cs
@@ -1,6 +1,7 @@
 ﻿namespace 阴阳易演.引用库
 {
     using System;
+    using System.Linq;
 
     public static class 枚举转换类<T> where T : struct
     {
@@ -8,39 +9,48 @@
         {
             return Enum.GetValues(typeof(T)).Length;
         }
+
         public static T[] 获取所有枚举()
         {
             return (T[])Enum.GetValues(typeof(T));
         }
+
         public static string[] 获取所有名称()
         {
             return Enum.GetNames(typeof(T));
         }
+
         public static string 获取名称(int 原数)
         {
-            var 进位数 = 获取枚举总数();
-            var 余数 = 原数 % 进位数;
-            var 序数 = 余数 < 0 ? 进位数 + 余数 : 余数;
-            return Enum.GetName(typeof(T), 序数);
+            var 枚举 = 获取枚举(原数);
+            return 获取名称(枚举);
         }
+
         public static string 获取名称(T 枚举)
         {
             return Enum.GetName(typeof(T), 枚举);
         }
+
         public static int 获取序数(string 名称)
         {
             var 枚举 = 获取枚举(名称);
             return 获取序数(枚举);
         }
+
         public static int 获取序数(T 枚举)
         {
             var 对象 = Enum.ToObject(typeof(T), 枚举);
             var 原数 = (int)对象;
+            if (包含(枚举))
+            {
+                return 原数;
+            }
             var 进位数 = 获取枚举总数();
             var 余数 = 原数 % 进位数;
             var 序数 = 余数 < 0 ? 进位数 + 余数 : 余数;
             return 序数;
         }
+
         public static T 获取枚举(string 名称)
         {
             try
@@ -52,16 +62,39 @@
                 throw new Exception($"枚举[{typeof(T).Name}]不包含名称{名称}");
             }
         }
+
         public static T 获取枚举(int 原数)
         {
+            if (包含(原数))
+            {
+                return (T)Enum.ToObject(typeof(T), 原数);
+            }
             var 进位数 = 获取枚举总数();
             var 余数 = 原数 % 进位数;
             var 序数 = 余数 < 0 ? 进位数 + 余数 : 余数;
             return (T)Enum.ToObject(typeof(T), 序数);
         }
+
         public static bool 尝试获取枚举(string 名称, out T 枚举)
         {
             return Enum.TryParse(名称, out 枚举);
         }
+
+        public static bool 包含(int 原数)
+        {
+            return 获取所有枚举().Any(t => (int)Enum.ToObject(typeof(T), t) == 原数);
+        }
+
+        public static bool 包含(T 枚举)
+        {
+            var 名称 = 获取名称(枚举);
+            return 包含(名称);
+        }
+
+        public static bool 包含(string 名称)
+        {
+            return 获取所有名称().Contains(名称);
+        }
+
     }
 }
